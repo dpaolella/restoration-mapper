f88ac2c907c3a932c4281ccde9e879ae488eb43a
Daniel Csonth <danielcsonth@Daniels-MacBook-Pro.local>
2020-05-02 20:46:28 -0400

    changed from disc to gen loss to see diff

--- disc_loss.py
+++ disc_loss.py
@@ -1,8 +1,8 @@
 
 
-    def discriminator_loss(self, Ra, loss_func, real, fake):
-        real_loss = 0
+    def generator_loss(self, Ra, loss_func, real, fake):
         fake_loss = 0
+        real_loss = 0
 
         if Ra and loss_func.__contains__('wgan') :
             #print("No exist [Ra + WGAN], so use the {} loss function".format(loss_func))
@@ -13,47 +13,41 @@
             fake_logit = (fake - tf.reduce_mean(real))
 
             if loss_func == 'lsgan' :
-                real_loss = tf.reduce_mean(tf.square(real_logit - 1.0))
-                fake_loss = tf.reduce_mean(tf.square(fake_logit + 1.0))
+                real_loss = tf.reduce_mean(tf.square(real_logit + 1.0))
+                fake_loss = tf.reduce_mean(tf.square(fake_logit - 1.0))
 
             if loss_func == 'gan' or loss_func == 'gan-gp' or loss_func == 'dragan' :
-                print('D ra_en sigmoid loss')
-                real_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=tf.ones_like(real), logits=real_logit))
-                fake_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=tf.zeros_like(fake), logits=fake_logit))
+                print('G ra_en sigmoid loss')
+                real_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=tf.zeros_like(real), logits=real_logit))
+                fake_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=tf.ones_like(fake), logits=fake_logit))
 
             if loss_func == 'ngan' :
-                print('D ra_en softmax loss')
-                real_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=tf.ones_like(real), logits=real_logit))
-                fake_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=tf.zeros_like(fake), logits=fake_logit))
+                print('G ra_en softmax loss')
+                real_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=tf.zeros_like(real), logits=real_logit))
+                fake_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=tf.ones_like(fake), logits=fake_logit))
 
             if loss_func == 'hinge' :
-                real_loss = tf.reduce_mean(tf.nn.relu(1.0 - real_logit))
-                fake_loss = tf.reduce_mean(tf.nn.relu(1.0 + fake_logit))
+                real_loss = tf.reduce_mean(tf.nn.relu(1.0 + real_logit))
+                fake_loss = tf.reduce_mean(tf.nn.relu(1.0 - fake_logit))
 
         else :
-            if loss_func == 'wgan-gp' or loss_func == 'wgan-lp' :
-                real_loss = -tf.reduce_mean(real)
-                fake_loss = tf.reduce_mean(fake)
+            if loss_func == 'wgan-gp' or loss_func == 'wgan-lp':
+                fake_loss = -tf.reduce_mean(fake)
 
             if loss_func == 'lsgan' :
-                real_loss = tf.reduce_mean(tf.square(real - 1.0))
-                fake_loss = tf.reduce_mean(tf.square(fake))
+                fake_loss = tf.reduce_mean(tf.square(fake - 1.0))
 
             if loss_func == 'gan' or loss_func == 'gan-gp' or loss_func == 'dragan' :
-                print('D ra_off sigmoid loss')
-                real_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=tf.ones_like(real), logits=real))
-                fake_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=tf.zeros_like(fake), logits=fake))
+                print('G ra_off sigmoid loss')
+                fake_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=tf.ones_like(fake), logits=fake))
 
             if loss_func == 'ngan':
-                print('D ra_off softmax loss')
-                real_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=tf.ones_like(real), logits=real))
-                fake_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=tf.zeros_like(fake), logits=fake))
+                print('G ra_off softmax loss')
+                fake_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=tf.ones_like(fake), logits=fake))
 
             if loss_func == 'hinge' :
-                real_loss = tf.reduce_mean(tf.nn.relu(1.0 - real))
-                fake_loss = tf.reduce_mean(tf.nn.relu(1.0 + fake))
+                fake_loss = -tf.reduce_mean(fake)
 
-        loss = real_loss + fake_loss
+        loss = fake_loss + real_loss
 
         return loss
-
